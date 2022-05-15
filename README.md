fc8a1c8264516acf64268e80da3562c750ccb69a
yefan <731080256@qq.com>
2022-05-15 11:13:19 +0800

    模板

---
+++ README.md
@@ -0,0 +1,22 @@
+# templateh5vue2
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run dev
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+# 本方案使用的移动端适配不支持Android 4.4 之下和 iOS 8 以下的版本，只支持安卓4.4以上的版本，+ios8以上版本的手机
+# 本方案帮忙引入了vant移动版的框架库，使用了自动引入的方式
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <div>
+<van-button type="primary">主要按钮</van-button>
+<van-button type="info">信息按钮</van-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Button } from 'vant';
+export default {
+  name: 'HomeView',
+  components: {
+    [Button.name]: Button,
+  }
+}
+</script>
+<style scoped>
+.home {
+  height: 500px;
+  width: 500px;
+  background-color: pink;
+}
+</style>>
